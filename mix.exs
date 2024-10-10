5a1150864ce47d26173c8155971388146f6c07ca
Cocoa <i@uwucocoa.moe>
2024-10-10 13:21:30 +0100

    updated configuration for `armv7l-linux-gnueabihf`
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- mix.exs
+++ mix.exs
@@ -148,7 +148,13 @@ defmodule Exqlite.MixProject do
           "riscv64-linux-musl" => "riscv64-linux-musl-",
           "x86_64-linux-gnu" => "x86_64-linux-gnu-",
           "aarch64-linux-gnu" => "aarch64-linux-gnu-",
-          "riscv64-linux-gnu" => "riscv64-linux-gnu-"
+          "riscv64-linux-gnu" => "riscv64-linux-gnu-",
+          "armv7l-linux-gnueabihf" => {
+            "arm-linux-gnueabihf-gcc",
+            "arm-linux-gnueabihf-g++",
+            "<%= cc %> -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard",
+            "<%= cxx %> -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard"
+          }
         },
         {:unix, :darwin} => %{
           :include_default_ones => true
